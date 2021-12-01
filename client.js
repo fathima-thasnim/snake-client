7d163e4699f6548760cdb1d183801bd3667f2221
fathima-thasnim <thasnimfathima492@gmail.com>
2021-12-01 22:25:24 +0000

    added intervals

--- client.js
+++ client.js
@@ -5,13 +5,34 @@ const connect = function () {
     host: "165.227.47.243",// IP address here,
     port: 50541// PORT number here,
   });
+
+  // interpret incomming data as text
   conn.setEncoding("utf8");
+
+  //receive data from server
   conn.on("connect", () => {
     console.log("Successfully connected to game server" )
   });
+
+
+  // sending name to the server
   conn.on('connect', () => {
-    conn.write('Name: FT')
-  })
+    conn.write('Name: FT');
+  });
+
+  
+  // move up snake with setIntervals
+  /*const move = setInterval(() => {
+    conn.write("Move: up");
+  },50);*/
+
+  // move up snake 5 times right after connection establishes each have 50msdelay
+  /*let timeDelay = 50;
+  for (let i = 0; i < 5; i++) {
+    setTimeout(() => conn.write("Move: up"), timeDelay);
+    timeDelay += 50
+  };*/
+
 
   return conn;
 };
