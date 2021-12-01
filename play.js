bef81e36735363c1d3ce56b4c74d20052c186089
fathima-thasnim <thasnimfathima492@gmail.com>
2021-12-01 23:14:36 +0000

    setup stdin

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const { connect } = require('./client');
+let connection
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  //Handling CTRL + C User Input
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (data) {
+  if (data === `\u0003`) {
+    console.log('Leaving...')
+    process.exit();
+  }
+  if (data === 'w') connection.write('Move: up ');
+  if (data === 'a') connection.write('Move: left ');
+  if (data === 'd') connection.write('Move: right ');
+  if (data === 's') connection.write('Move: down ');
+}
 
 
 console.log("Connecting ...");
